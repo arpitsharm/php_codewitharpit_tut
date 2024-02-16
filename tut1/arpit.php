5aba987e7986bf318942d33b6b454670f8898b92
arpitsharm <150226804+arpitsharm@users.noreply.github.com>
2024-02-16 19:23:48 +0530

    first commit

---
+++ tut1/arpit.php
@@ -0,0 +1,4 @@
+<?php
+echo "This is Arpit.php";
+echo 249+288;
+?>
---
+++ tut1/index.php
@@ -0,0 +1,7 @@
+<?php
+// echo "Hello World";
+// echo "<div></div>";
+// echo 29+28;
+
+// echo "<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perferendis blanditiis, enim officia ut accusamus id modi qui possimus itaque ipsa consequatur quis, odio maxime perspiciatis tempora soluta tenetur adipisci?</h1>";
+?>
